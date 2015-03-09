c887d94af98f1c50355493e71f9bd4d684817f3f
ryanknights <hello@ryanknights.co.uk>
2015-03-09 19:37:49 +0000

    Restructuring directory

---
+++ README.md
@@ -0,0 +1,1 @@
+Wordpress Options Page Creator
--- example.php
+++ example.php
@@ -1,97 +1,27 @@
 <?php
-function create_tabbed_options_page ()
-{
-	$optionsPage = new tmsOptionsCreator(array(
 
-		'title' => 'Tabbed Options',
-		'key'   => 'tabbed_options',
-		'tabs'  => array(
-			'tab-one'   => 'Tab One',
-			'tab-two'   => 'Tab Two'
-		)
-	));
+	if (!defined( 'ABSPATH'))
+	{
+		exit();
+	}
+	
+	function create_tabbed_options_page ()
+	{
+		$optionsPage = new tmsOptionsCreator(array(
 
-	$optionsPage->addSection(array(
-		'id' => 'section_one',
-		'title' => 'Section One',
-		'content' => 'This is section one',
-		'tab'     => 'tab-one'
-	));
-
-		$optionsPage->addField(array(
-			'id'      => 'test_field',
-			'title'   => 'Text Input',
-			'type'    => 'text',
-			'section' => 'section_one',
-			'tab'     => 'tab-one'
-		));	
-
-		$optionsPage->addField(array(
-			'id'      => 'test_field_2',
-			'title'   => 'Text Input 2',
-			'type'    => 'text',
-			'section' => 'section_one',
-			'tab'     => 'tab-one'
+			'title' => 'Tabbed Options',
+			'key'   => 'tabbed_options',
+			'tabs'  => array(
+				'tab-one'   => 'Tab One',
+				'tab-two'   => 'Tab Two'
+			)
 		));
 
-		$optionsPage->addField(array(
-			'id'      => 'test_field_3',
-			'title'   => 'TextArea',
-			'type'    => 'textarea',
-			'section' => 'section_one',
-			'tab'     => 'tab-one'
-		));		
-
-	$optionsPage->addSection(array(
-		'id' => 'section_one',
-		'title' => 'Section One',
-		'content' => 'This is section one',
-		'tab'     => 'tab-two'
-	));
-
-		$optionsPage->addField(array(
-			'id'      => 'test_field',
-			'title'   => 'Text Input',
-			'type'    => 'text',
-			'section' => 'section_one',
-			'tab'     => 'tab-two'
-		));	
-
-		$optionsPage->addField(array(
-			'id'      => 'test_field_2',
-			'title'   => 'Text Input 2',
-			'type'    => 'text',
-			'section' => 'section_one',
-			'tab'     => 'tab-two'
-		));	
-
-		$optionsPage->addField(array(
-			'id'      => 'test_field_3',
-			'title'   => 'Select Box',
-			'type'    => 'select',
-			'values'  => array('one', 'two', 'three'),
-			'section' => 'section_one',
-			'tab'     => 'tab-two'
-		));					
-
-
-}
-
-add_action('admin_menu', 'create_tabbed_options_page');
-
-function create_static_options_page ()
-{	
-	$optionsPage = new tmsOptionsCreator(array(
-
-		'title' => 'Static Options',
-		'key'   => 'static_options'
-	));
-
 		$optionsPage->addSection(array(
-
 			'id' => 'section_one',
 			'title' => 'Section One',
-			'content' => 'This is section one'
+			'content' => 'This is section one',
+			'tab'     => 'tab-one'
 		));
 
 			$optionsPage->addField(array(
@@ -99,27 +29,46 @@ function create_static_options_page ()
 				'title'   => 'Text Input',
 				'type'    => 'text',
 				'section' => 'section_one',
+				'tab'     => 'tab-one'
+			));	
+
+			$optionsPage->addField(array(
+				'id'      => 'test_field_2',
+				'title'   => 'Text Input 2',
+				'type'    => 'text',
+				'section' => 'section_one',
+				'tab'     => 'tab-one'
 			));
 
-		$optionsPage->addSection(array(
+			$optionsPage->addField(array(
+				'id'      => 'test_field_3',
+				'title'   => 'TextArea',
+				'type'    => 'textarea',
+				'section' => 'section_one',
+				'tab'     => 'tab-one'
+			));		
 
-			'id' => 'section_two',
-			'title' => 'Section Two',
-			'content' => 'This is section two'
+		$optionsPage->addSection(array(
+			'id' => 'section_one',
+			'title' => 'Section One',
+			'content' => 'This is section one',
+			'tab'     => 'tab-two'
 		));
 
 			$optionsPage->addField(array(
 				'id'      => 'test_field',
 				'title'   => 'Text Input',
 				'type'    => 'text',
-				'section' => 'section_two',
-			));
+				'section' => 'section_one',
+				'tab'     => 'tab-two'
+			));	
 
 			$optionsPage->addField(array(
 				'id'      => 'test_field_2',
-				'title'   => 'TextArea',
-				'type'    => 'textarea',
-				'section' => 'section_two',
+				'title'   => 'Text Input 2',
+				'type'    => 'text',
+				'section' => 'section_one',
+				'tab'     => 'tab-two'
 			));	
 
 			$optionsPage->addField(array(
@@ -127,52 +76,109 @@ function create_static_options_page ()
 				'title'   => 'Select Box',
 				'type'    => 'select',
 				'values'  => array('one', 'two', 'three'),
-				'section' => 'section_two'
+				'section' => 'section_one',
+				'tab'     => 'tab-two'
+			));					
+
+
+	}
+
+	add_action('admin_menu', 'create_tabbed_options_page');
+
+	function create_static_options_page ()
+	{	
+		$optionsPage = new tmsOptionsCreator(array(
+
+			'title' => 'Static Options',
+			'key'   => 'static_options'
+		));
+
+			$optionsPage->addSection(array(
+
+				'id' => 'section_one',
+				'title' => 'Section One',
+				'content' => 'This is section one'
 			));
 
-			$optionsPage->addField(array(
-				'id'      => 'animals',
-				'title'   => 'Animals',
-				'type'    => 'checkbox',
-				'values'  => array(
-					'Dog'  => 'dog',
-					'Cat'  => 'cat',
-					'Frog' => 'frog'
-				),
-				'section' => 'section_two'
-			));	
+				$optionsPage->addField(array(
+					'id'      => 'test_field',
+					'title'   => 'Text Input',
+					'type'    => 'text',
+					'section' => 'section_one',
+				));
 
-			$optionsPage->addField(array(
-				'id'      => 'enabled',
-				'title'   => 'Enabled?',
-				'type'    => 'checkbox',
-				'values'  => array(
-					'Am i enabled' => true,
-				),
-				'section' => 'section_two'
+			$optionsPage->addSection(array(
+
+				'id' => 'section_two',
+				'title' => 'Section Two',
+				'content' => 'This is section two'
 			));
 
-			$optionsPage->addField(array(
-				'id'      => 'age',
-				'title'   => 'Age',
-				'type'    => 'radio',
-				'values'  => array(
-					'10-20' => '10_20',
-					'20-30' => '20_30',
-					'30_40' => '30_40'
-				),
-				'section' => 'section_two'
-			));
+				$optionsPage->addField(array(
+					'id'      => 'test_field',
+					'title'   => 'Text Input',
+					'type'    => 'text',
+					'section' => 'section_two',
+				));
 
-			$optionsPage->addField(array(
-				'id'       => 'intro_text',
-				'title'    => 'Introduction Text',
-				'type'     => 'tinymce',
-				'settings' => array('textarea_rows' => 10),
-				'section'  => 'section_two'
-			));																			
-}
+				$optionsPage->addField(array(
+					'id'      => 'test_field_2',
+					'title'   => 'TextArea',
+					'type'    => 'textarea',
+					'section' => 'section_two',
+				));	
 
-add_action('admin_menu', 'create_static_options_page');
+				$optionsPage->addField(array(
+					'id'      => 'test_field_3',
+					'title'   => 'Select Box',
+					'type'    => 'select',
+					'values'  => array('one', 'two', 'three'),
+					'section' => 'section_two'
+				));
+
+				$optionsPage->addField(array(
+					'id'      => 'animals',
+					'title'   => 'Animals',
+					'type'    => 'checkbox',
+					'values'  => array(
+						'Dog'  => 'dog',
+						'Cat'  => 'cat',
+						'Frog' => 'frog'
+					),
+					'section' => 'section_two'
+				));	
+
+				$optionsPage->addField(array(
+					'id'      => 'enabled',
+					'title'   => 'Enabled?',
+					'type'    => 'checkbox',
+					'values'  => array(
+						'Am i enabled' => true,
+					),
+					'section' => 'section_two'
+				));
+
+				$optionsPage->addField(array(
+					'id'      => 'age',
+					'title'   => 'Age',
+					'type'    => 'radio',
+					'values'  => array(
+						'10-20' => '10_20',
+						'20-30' => '20_30',
+						'30_40' => '30_40'
+					),
+					'section' => 'section_two'
+				));
+
+				$optionsPage->addField(array(
+					'id'       => 'intro_text',
+					'title'    => 'Introduction Text',
+					'type'     => 'tinymce',
+					'settings' => array('textarea_rows' => 10),
+					'section'  => 'section_two'
+				));																			
+	}
+
+	add_action('admin_menu', 'create_static_options_page');
 
 ?>
